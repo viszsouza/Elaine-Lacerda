46033d9de7e7d3a439f0d16c38394df4b087080e
vizssouza <vini.nelso@gmail.com>
2025-04-28 12:13:54 -0300

    Ajustando Footer e adicionando animações
    Finalizando site

--- JS/script.js
+++ JS/script.js
@@ -101,3 +101,19 @@ function enviarParaWhatsApp() {
     
     window.open(url, "_blank");
 }
+
+// Animações
+// Coletando o observador da página
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting === true){
+            entry.target.classList.add('show')
+        } 
+    })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach( (element) => myObserver.observe(element))
